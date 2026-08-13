47b18fbb3af68c807c0c069a2ac753599828592d
zoomdong <1344492820@qq.com>
2026-08-13 15:16:25 +0800

    chore: simplify utlint config

--- utlint.config.ts
+++ utlint.config.ts
@@ -1,25 +1,22 @@
-import { defineConfig, globalIgnores } from '@utoo/lint/config';
+import { defineConfig } from '@utoo/lint/config';
 
-export default defineConfig(
-  globalIgnores(['candidate-artifacts', 'node_modules', 'workspace']),
-  {
-    files: [
-      'utlint.config.ts',
-      'src/**/*.{js,jsx,ts,tsx,mjs,cjs}',
-      'scripts/**/*.{js,jsx,ts,tsx,mjs,cjs}',
-      'test/**/*.{js,jsx,ts,tsx,mjs,cjs}',
-      'fixtures/**/*.{js,jsx,ts,tsx,mjs,cjs}',
-    ],
-    rules: {
-      'no-debugger': 'error',
-      'no-unused-vars': 'error',
-      'prefer-const': 'error',
-      eqeqeq: 'error',
-      'use-isnan': 'error',
-      'valid-typeof': 'error',
-      'no-constant-condition': 'error',
-      'no-dupe-keys': 'error',
-      'no-duplicate-imports': 'error',
-    },
+export default defineConfig({
+  files: [
+    'utlint.config.ts',
+    'src/**/*.{js,jsx,ts,tsx,mjs,cjs}',
+    'scripts/**/*.{js,jsx,ts,tsx,mjs,cjs}',
+    'test/**/*.{js,jsx,ts,tsx,mjs,cjs}',
+    'fixtures/**/*.{js,jsx,ts,tsx,mjs,cjs}',
+  ],
+  rules: {
+    'no-debugger': 'error',
+    'no-unused-vars': 'error',
+    'prefer-const': 'error',
+    eqeqeq: 'error',
+    'use-isnan': 'error',
+    'valid-typeof': 'error',
+    'no-constant-condition': 'error',
+    'no-dupe-keys': 'error',
+    'no-duplicate-imports': 'error',
   },
-);
+});
